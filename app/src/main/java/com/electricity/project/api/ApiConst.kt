b86ae96b4e9092113b9679d864a6ef6deaaaf6f0
Daniel Glazer <danielglazer200026@gmail.com>
2024-01-25 21:59:36 +0100

    SZOZE-40 (#2)
    * SZOZE-40 Change structure of project
    
    * SZOZE-40 Add getting data with Bearer Token
    
    * SZOZE-40 Fix optimization
    
    * SZOZE-40 Change username to logo with system name
    
    * SZOZE-40 Change address to localhost
    
    * SZOZE-40 Change application icon
    
    * SZOZE-40 Change to emulator address

--- app/src/main/java/com/electricity/project/api/ApiConst.kt
+++ app/src/main/java/com/electricity/project/api/ApiConst.kt
@@ -7,7 +7,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 
 object ApiConst {
+    //Emulator address
+   // private const val LOGIC_URL: String = "http://10.0.2.2:8084/api/"
+
+    // Production address
     private const val LOGIC_URL: String = "http://localhost:8084/api/"
+
 
     val retrofitBasicBuilder: Retrofit.Builder = Retrofit.Builder()
         .baseUrl(LOGIC_URL)
